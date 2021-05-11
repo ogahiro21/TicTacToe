648500f2db8b5a24de726455d9e769f02d6d77b5
ogahiro21 <hiromasaogawaa@gmail.com>
2021-05-11 16:44:22 +0900

    Initial app

---
+++ tictactoe/frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+export const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("app"));

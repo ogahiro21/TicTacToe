7dcbb4a3122579eab1461ee8ccbbbf905ec4a52a
ogahiro21 <hiromasaogawaa@gmail.com>
2021-05-31 10:38:09 +0900

    Update App.js

--- tictactoe/frontend/src/components/App.js
+++ tictactoe/frontend/src/components/App.js
@@ -5,12 +5,13 @@ import { Board } from './Board'
 
 
 // FontAwsomeのインポート
+/*
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/pro-solid-svg-icons'
 import { far } from '@fortawesome/pro-regular-svg-icons'
 import { fal } from '@fortawesome/pro-light-svg-icons'
 import { fad } from '@fortawesome/pro-duotone-svg-icons'
-library.add(fas, far, fal, fad)
+library.add(fas, far, fal, fad)*/
 
 export const App = () => {
   const boardBody = useRef(null)
